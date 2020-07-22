600881b3e7d3f8bf93da9531e9dd243a51c10c51
Aniketrana <ranaannu93@gmail.com>
2020-07-22 14:47:34 +0530

    Add project files.

---
+++ MVC_AppWithDB/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApp.Models;
+using MyApp.Db.DbOperation;
+
+namespace MVC_AppWithDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmpRepository repository = null;
+
+        public HomeController()
+        {
+            repository = new EmpRepository();
+
+        }
+        // GET: Home
+        public ActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmpModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //below line add in edmx model cs file in top namespace
+                //using SqlProviderServices = System.Data.Entity.SqlServer.SqlProviderServices;
+                
+                int id= repository.AddEmp(model);
+                if (id > 0)
+                {
+                    ModelState.Clear();
+                    ViewBag.Issuccess = "Data Added";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MyApp.Db/DbOperation/EmpRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp.Models;
+
+namespace MyApp.Db.DbOperation
+{
+    public class EmpRepository
+    {
+        public int AddEmp(EmpModel model)
+        {
+            using (var context = new AniketEntities())
+            {
+                tblEmp emp = new tblEmp()
+                {
+                    FirstName = model.FirstName,
+                    LastName= model.LastName,
+                    Email= model.Email,
+                    Code = model.Code
+                    
+                };
+                if (model.Address != null)
+                {
+                    emp.tblAddress = new tblAddress()
+                    {
+                        Details = model.Address.Details,
+                        Country = model.Address.Country,
+                        State= model.Address.State,
+                        City= model.Address.City,
+                    };
+
+                }
+                context.tblEmp.Add(emp);
+                context.SaveChanges();
+                return emp.Id;
+            }
+        }
+    }
+}
